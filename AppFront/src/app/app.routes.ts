5a4d6d1b0b882cf5c8949e9fafdaf4db44c16075
JuanMPaola <juanmpaola25@gmail.com>
2023-12-05 20:14:21 -0300

    creando routes

--- AppFront/src/app/app.routes.ts
+++ AppFront/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { ProductsListComponent } from './components/products/products-list/products-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'product', component: ProductsListComponent},
+    /* {path: 'products/:productId', component: } */
+];
